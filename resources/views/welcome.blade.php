b61aa435bcf1925e8814ab402faba9fc573ef3e0
smile <smile@qq.com>
2020-03-10 10:53:14 +0000

    laravel auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    dev-shop
                 </div>
 
                 <div class="links">
