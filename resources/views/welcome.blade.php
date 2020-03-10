48ae69f663fefe123597559f842791819fd67b02
smile <smile@qq.com>
2020-03-10 11:16:58 +0000

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    dev-shop
+                    {{env('APP_NAME')}}
                 </div>
 
                 <div class="links">
